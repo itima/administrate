faf301e5bc25148a71976bf4cede9ad36d5569a2
Alexey Vorobiev <alexey@itima.ru>
2016-02-29 19:49:56 +0300

    fixed belongs_to

--- lib/administrate/field/belongs_to.rb
+++ lib/administrate/field/belongs_to.rb
@@ -6,29 +6,37 @@ module Administrate
       def self.permitted_attribute(attr)
         :"#{attr}_id"
       end
-
+  
       def permitted_attribute
         self.class.permitted_attribute(attribute)
       end
-
+  
       def associated_resource_options
-        [nil] + candidate_resources.map do |resource|
+        resources = candidate_resources.map do |resource|
           [display_candidate_resource(resource), resource.id]
         end
+        if options.fetch(:allow_nil, true)
+          resources = [nil] + resources
+        else
+          resources
+        end
       end
-
+  
       def selected_option
         data && data.id
       end
-
+  
       private
-
+  
       def candidate_resources
         associated_class.all
       end
-
+  
       def display_candidate_resource(resource)
         associated_dashboard.display_resource(resource)
+      rescue StandardError
+        resource.to_s
+        #"#{resource.to_s} [id:#{resource.id}]"
       end
     end
   end
